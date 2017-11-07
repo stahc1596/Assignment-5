ac0de12a8b9fbdb909b76247fff1d182b319d1d8
stahc1596 <stahc1596@HRH-IC0027341.wrdsb.ca>
2017-11-07 10:15:11 -0500

    Getting the program to remove a number

--- src/assignment5/OrderedList.java
+++ src/assignment5/OrderedList.java
@@ -12,87 +12,79 @@ public class OrderedList {
 
     private IntNode head;
     private int numItems;
-    
-    public OrderedList(){
+
+    public OrderedList() {
         head = null;
         numItems = 0;
     }
-    
-    public void add(int num){
+
+    public void add(int num) {
         //Start at beginning of list
         IntNode node = head;
+        IntNode prev = null;
         //See if you're at the start
-        if(node == null){
+        if (node == null) {
             IntNode temp = new IntNode(num);
             head = temp;
-        }else{
-        //Travel to the end
-            int count = 0;
-            int tot = 0;
-            while(node.getNext() != null){
-                node = node.getNext();
-                tot++;
+        } else {
+            //Travel to the end
+
+            while (node != null) {
+                // need to insert BEFORE node
+                if (num < node.getNum()) {
+
+                    IntNode temp = new IntNode(num);
+                    temp.setNext(node);
+                    if (prev != null) {
+                        prev.setNext(temp);
+                    }else{
+                        head = temp;
+                    }
+                    break;
+                } else {
+                    prev = node;
+                    node = node.getNext();
+                }
+                
             }
-            //node is the last node in the list
-            IntNode temp = new IntNode(num);
-            //Insert into list
-            node.setNext(temp);
-            node = null;
-            while(num < node.getNum()){
-                node.getNext();
-                count++;
+            if(node == null){
+                IntNode temp = new IntNode(num);
+                prev.setNext(temp);
             }
-            int store = node.getNum();
+            
+            
         }
         numItems++;
     }
-    
-    public int size(){
+
+    public int size() {
         return numItems;
     }
-    
-    public boolean isEmpty(){
+
+    public boolean isEmpty() {
         return numItems == 0;
     }
-    
-    public int get(int pos){
+
+    public int get(int pos) {
         IntNode node = head;
         //Move the number of times
-        for(int i = 0; i < pos; i++){
+        for (int i = 0; i < pos; i++) {
             node = node.getNext();
         }
         return node.getNum();
     }
-    
-    public void remove(int pos){
-        //If removing from start
-        if(pos == 0){
-            head = head.getNext();
-        }else if(pos == numItems - 1){
-            //Removing from the end
-            IntNode node = head;
-            //Moving to the second last item
-            for(int i = 0; i < numItems; i++){
-                node = node.getNext();
-            }
-            //Sever the link
-            node.setNext(null);
-        }else{
-            IntNode node = head;
-            //Move to the spot just before
-            for(int i = 0; i < pos - 1; i++){
-                node = node.getNext();
-            }
-            //The node to remove
-            IntNode toRemove = node.getNext();
-            //It's next node
-            IntNode next = toRemove.getNext();
-            //Set all links
-            node.setNext(next);
-            toRemove.setNext(null);
+
+    public void remove(int num) {
+        IntNode node = head;
+        IntNode prev = null;
+        while(num != node.getNum()){
+            prev = node;
+            node.getNext();
         }
+        
         numItems--;
     }
+
     /**
      * @param args the command line arguments
      */
@@ -102,7 +94,7 @@ public class OrderedList {
         list.add(8);
         list.add(5);
         list.add(1);
-        for(int i = 0; i < 4; i++){
+        for (int i = 0; i < 4; i++) {
             System.out.println(list.get(i));
         }
     }
